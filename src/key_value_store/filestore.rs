b81a05382585b4a5eac25226961a1450db226600
Sriram Venkatachalam Ramaswamy <android.svr@gmail.com>
2025-09-21 22:55:47 -0700

    [#4] Testing for file r/w and improvments
    Adds improvements to file R/W and adds testing for the same.

--- src/key_value_store/filestore.rs
+++ src/key_value_store/filestore.rs
@@ -20,7 +20,7 @@ fn key_value_store_to_msg(k: KeyValueStore) -> KeyValueStoreMsg {
 }
 
 fn msg_to_key_value_store(m: KeyValueStoreMsg) -> KeyValueStore {
-    let mut store =  KeyValueStore::new(&m.name);
+    let mut store =  KeyValueStore::new(m.name.as_str());
     for pair in m.values {
         store.add(KeyValuePair::new(
             &pair.0, &pair.1));
@@ -30,8 +30,7 @@ fn msg_to_key_value_store(m: KeyValueStoreMsg) -> KeyValueStore {
 
 pub fn write_to_file(store: KeyValueStore, target_file: &str) -> Result<(), errors::RWError> {
     let msg = key_value_store_to_msg(store);
-    let mut bytes = vec![];
-    msg.encode_length_delimited(&mut bytes).unwrap();
+    let bytes = msg.encode_to_vec();
     let mut file;
     match File::create(target_file) {
         Ok(f) => { file = f; }
@@ -41,7 +40,9 @@ pub fn write_to_file(store: KeyValueStore, target_file: &str) -> Result<(), erro
         })}
     };
     match file.write_all(&bytes) {
-        Ok(_) => { }
+        Ok(_) => { 
+          println!("bytes: {:?}, len: {:?}", bytes, bytes.len());
+        }
         Err(e) => { return Err(RWError
             { kind_: ErrorKind::FileWriteError, context_: e.to_string() })}
     };
@@ -56,14 +57,22 @@ pub fn read_from_file(src_file: &str) -> Result<KeyValueStore, errors::RWError> 
             kind_: ErrorKind::FileOpenError, context_: e.to_string()
         })}
     };
-    let mut buf =  vec![];
+    let mut buf =  vec![0;1024];
+    let n_bytes: usize;
     match file.read(&mut buf) {
-        Ok(_) => {},
+        Ok(n_b) => { if n_b == 0 {
+           panic!("Empty file!!");
+        } else {
+            println!("n_bytes: {:?}", n_b);
+            println!(
+                "buf: {:?}, len: {:?}", buf, buf.len());
+            n_bytes = n_b;
+        }},
         Err(e) => {return Err(RWError {
             kind_: ErrorKind::FileReadError,
             context_: e.to_string() })}
     };
-    match KeyValueStoreMsg::decode_length_delimited(&buf[..]) {
+    match KeyValueStoreMsg::decode(&buf[..n_bytes]) {
         Ok(msg) => Ok(msg_to_key_value_store(msg)),
         Err(e) => Err(RWError { 
             kind_: ErrorKind::DataDecodeError,
@@ -78,9 +87,20 @@ mod tests {
 
     fn create_simple_kv_store() -> KeyValueStore {
         let mut kvs = KeyValueStore::new("test");
-        kvs.add(KeyValuePair::new("Hello", "Test"));
-        kvs.add(KeyValuePair::new("Goodbye", "Test"));
+        kvs.add(KeyValuePair::new("Hello", "Value1"));
+        kvs.add(KeyValuePair::new("Goodbye", "Value2"));
         return kvs;
+    }
+
+    fn equality_test(lhs: KeyValueStore, rhs: KeyValueStore) {
+        assert_eq!(lhs.name(), rhs.name(), "Name Mismatch!");
+        for (k, v) in lhs.all() {
+            if let Some(val) = rhs.get(&k.clone()) {
+                assert_eq!(v, val.value(), "Mismatch for key: {:?}", k);
+            } else {
+                assert!(false, "No value found for key {:?}!", k);
+            }
+        }
     }
 
     #[test]
@@ -89,14 +109,24 @@ mod tests {
         let msg = key_value_store_to_msg(kvs.clone());
         let kvs_2 = msg_to_key_value_store(msg);
 
-        assert_eq!(kvs.name(), kvs_2.name());
+        equality_test(kvs, kvs_2);
+    }
 
-        for (k, v) in kvs.all() {
-            if let Some(val) = kvs_2.get(&k.clone()) {
-                assert_eq!(v, val.value());
-            } else {
-                assert!(false, "No value found for key {:?}!", k);
-            }
+    #[test]
+    fn test_file_io() {
+        let kvs = create_simple_kv_store();
+        let file_name = "/tmp/test.buf";
+        match write_to_file(kvs.clone(), file_name) {
+            Ok(_) => {},
+            Err(e) => { panic!("Write error: {:?}!", e); }
         }
+        let kvs2: KeyValueStore;
+        match read_from_file(file_name) {
+            Ok(k) => {
+                kvs2 = k;
+            },
+            Err(e) => { panic!("Read error: {:?}!", e); }
+        }
+        equality_test(kvs, kvs2);
     }
 }
